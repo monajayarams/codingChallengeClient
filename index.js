eccdea88f1a9c34c220ea13f763a812f2dddaefb
MONA JAYARAM <monajayarams@gmail.com>
2020-01-15 12:12:40 +0530

    updated the import statement

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './src/scripts/app.jsx';
+import App from './src/scripts/App.jsx';
 
 ReactDOM.render(<App />, document.getElementById('app'));
